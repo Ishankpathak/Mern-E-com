d4823bf7153325376f3c2b19b5044aa94b5696d7
Ishankpathak <pathakishank4@gmail.com>
2023-06-02 17:58:33 +0530

    Filters added

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,6 @@
 import slugify from "slugify";
 import productModel from "../models/productModel.js";
+import categoryModel from "../models/categoryModel.js";
 import fs from "fs";
 export const createProductController = async (req, res) => {
   try {
@@ -259,6 +260,50 @@ export const searchProductController = async (req, res) => {
       success: false,
       message: "Error in searching Product API",
       error,
+    });
+  }
+};
+
+//similar products
+export const relatedProductController = async (req, res) => {
+  try {
+    const { pid, cid } = req.params;
+    const products = await productModel
+      .find({
+        category: cid,
+        _id: { $ne: pid },
+      })
+      .select("-photo")
+      .limit(3)
+      .populate("category");
+    res.status(200).send({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "error while getting related product",
+    });
+  }
+};
+
+export const productCategoryController = async (req, res) => {
+  try {
+    const category = await categoryModel.findOne({ slug: req.params.slug });
+    const products = await productModel.find({ category }).populate("category");
+    res.status(200).send({
+      success: true,
+      category,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      error,
+      message: "Error while getting products",
     });
   }
 };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -5,10 +5,12 @@ import {
   deleteProductController,
   getProductController,
   getSingleProductController,
+  productCategoryController,
   productCountController,
   productFiltersController,
   productListController,
   productPhotoController,
+  relatedProductController,
   searchProductController,
   updateProductController,
 } from "../controllers/productController.js";
@@ -56,5 +58,11 @@ router.get("/product-list/:page", productListController);
 
 //search Product
 router.get("/search/:keyword", searchProductController);
+
+//similar products
+router.get("/related-product/:pid/:cid", relatedProductController);
+
+//category wise product
+router.get(`/product-category/:slug`, productCategoryController);
 
 export default router;
